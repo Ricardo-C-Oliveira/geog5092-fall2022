8342634f5ab3c4d11c74f904aa884c1bbdf71590
ricardo-c-oliveira-ucdenver <ricardo.oliveira@ucdenver.edu>
2022-11-10 17:54:03 -0700

    Added check .env file handler

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import os
+import os
+
+if not os.path.exists('.env'):
+    print("A .env file should exists with content: PERSONAL_TOKEN")
+    os._exit(1)
 
 with open('.env') as env_file:
     contents = env_file.readlines()
